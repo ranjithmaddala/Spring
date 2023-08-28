35817af07bc34a94aeab9f54884686516559c113
ranjithmaddala <84985922+ranjithmaddala@users.noreply.github.com>
2023-08-28 11:31:44 +0530

    deleted

--- XL-Demo/src/main/java/com/example/repository/XlEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import com.example.entity.XlEntity;
-
-public interface XlEntityRepository extends JpaRepository<XlEntity, Integer>{
-	
-}
